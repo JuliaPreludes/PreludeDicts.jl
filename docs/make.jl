3d58f98aae88e96ae1d91d990b4b6eb09ec3edc3
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-14 21:44:19 -0700

    Build documentation in CI (#5)

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,29 @@ using PreludeDicts
 makedocs(
     sitename = "PreludeDicts",
     format = Documenter.HTML(),
-    modules = [PreludeDicts]
+    modules = [PreludeDicts],
+    strict = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :doctest,
+        :eval_block,
+        :example_block,
+        :footnote,
+        :linkcheck,
+        :meta_block,
+        # :missing_docs,
+        :parse_error,
+        :setup_block,
+    ],
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/JuliaPreludes/PreludeDicts.jl",
+    devbranch = "main",
+    push_preview = true,
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.deploydocs
+)
